20ca93e654badf385c7c56195672bf67a43732bc
IwamotoKogan <141932936+IwamotoKogan@users.noreply.github.com>
2024-01-10 19:12:34 +0100

    Update popup3.js

--- popup3.js
+++ popup3.js
@@ -37,7 +37,7 @@ if (savedItems.length > 0) {
             <td class="product-name ">cena vašeg elementa je<strong> ${item.price}</strong>  evra</td>
                
 <<button type="button" class="btn btn-danger delete-button custom-delete-button" data-index="${index}" data-toggle="modal" data-target="#exampleModalCenter">
-  Launch demo modal
+ Izbriši
 </button>
 
 
